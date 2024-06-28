88e2c13651e4d3c86a818838bc700dad9b4ce89e
KenzaH7 <kenzahadjsassi190@gmail.com>
2024-06-28 18:21:32 +0200

    QuizUpVol1

---
+++ JS/script.js
@@ -0,0 +1,101 @@
+const questions = [
+  {
+    question: "what's the capital of tunisia?",
+    answers: [
+      { text: "Tunis", correct: true },
+      { text: "kairouan", correct: false },
+      { text: "Gabes", correct: false },
+      { text: "Sousse", correct: false },
+    ],
+  },
+  {
+    question: "which is the smallest continent in the world??",
+    answers: [
+      { text: "Australia", correct: true },
+      { text: "Europe", correct: false },
+      { text: "Asia", correct: false },
+      { text: "Africa", correct: false },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+var currentQuestionIndex = 0;
+var score = 0;
+function startQuizz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+}
+
+function showQuestion() {
+  resetState();
+  var currentQuestion = questions[currentQuestionIndex];
+  var questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML =
+    "you sccored " + score + " out of " + questions.length;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuizz();
+  }
+});
+
+showQuestion();
